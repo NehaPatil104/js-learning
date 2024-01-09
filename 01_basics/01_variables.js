a3e1df676ffcb3d345ca2a76dbd5ea99f49d6cbc
Neha Patil <nehaspatil125000@gmail.com>
2024-01-09 05:01:10 +0000

    Data Type file added

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,6 +1,6 @@
-const accountId = 614040
-let accountEmail = "neha@google.com"
-var accountPassword = "123"
+const accountId = 614040 // declare a constant variable
+let accountEmail = "neha@google.com" // has a block-level scope
+var accountPassword = "123" // public scope throught the program
 accountCity = "Pune"
 let accountState
 
@@ -10,6 +10,15 @@ accountEmail = "hc@hc.com"
 accountPassword = "456"
 accountCity = "Nandurbar"
 
+if(18 == 18){
+    let myVar = 10
+    var myVar2 = 20
+    accountPassword = "789"
+    console.log(myVar,myVar2)
+}
+
+console.log(myVar2)
+
 console.log(accountId);
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
 
@@ -17,3 +26,4 @@ console.table([accountId, accountEmail, accountPassword, accountCity, accountSta
 Prefer not to use var
 because of issue in block scope and functional scope
 */
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // to treat all JS code as newer version of JS
+
+// alert("HELLOOO") // we are using nodejs, not browser
+
+let name = "NeHa"
+let age = 18
+let isLoggedIn = false
+
+// Primitive Data Types
+// number => 2 to power 53
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+// Object
+console.log(typeof "Neha")
+console.log(typeof null)
+console.log(typeof undefined)
